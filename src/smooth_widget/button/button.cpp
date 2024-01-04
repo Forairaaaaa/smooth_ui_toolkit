fdf0bb746992672028c0d19302160d343b819784
Forairaaaaa <applesyqd@outlook.com>
2024-01-04 15:50:58 +0800

    block button to todo

--- src/smooth_widget/button/button.cpp
+++ src/smooth_widget/button/button.cpp
@@ -1,39 +1,14 @@
-/**
- * @file button.cpp
- * @author Forairaaaaa
- * @brief 
- * @version 0.1
- * @date 2024-01-03
- * 
- * @copyright Copyright (c) 2024
- * 
- */
-#include "button.h"
+// /**
+//  * @file button.cpp
+//  * @author Forairaaaaa
+//  * @brief 
+//  * @version 0.1
+//  * @date 2024-01-03
+//  * 
+//  * @copyright Copyright (c) 2024
+//  * 
+//  */
+// #include "button.h"
+// using namespace SmoothUIToolKit::SmoothWidget;
 
-
-using namespace SmoothUIToolKit::SmoothWidget;
-
-
-void Button::update(std::uint32_t currentTime)
-{
-    _onUpdate();
-}
-
-
-void Button::hover(int x, int y)
-{
-    _onHover();
-}
-
-
-void Button::drag(int x, int y)
-{
-    _onDrag();
-}
-
-
-void Button::drop()
-{
-    _onDrop();
-}
 
--- src/smooth_widget/button/button.h
+++ src/smooth_widget/button/button.h
@@ -1,72 +1,58 @@
-/**
- * @file button.h
- * @author Forairaaaaa
- * @brief 
- * @version 0.1
- * @date 2024-01-03
- * 
- * @copyright Copyright (c) 2024
- * 
- */
-#pragma once
-#include "../base/base.h"
+// /**
+//  * @file button.h
+//  * @author Forairaaaaa
+//  * @brief 
+//  * @version 0.1
+//  * @date 2024-01-03
+//  * 
+//  * @copyright Copyright (c) 2024
+//  * 
+//  */
+// #pragma once
+// #include "../base/base.h"
 
 
-namespace SmoothUIToolKit 
-{ 
-    namespace SmoothWidget 
-    {
-        class Button;
+// namespace SmoothUIToolKit 
+// { 
+//     namespace SmoothWidget 
+//     {
+//         class Button;
 
-        typedef void (*ButtonOnUpdateCallback)(Button*);
-        typedef void (*ButtonOnhoverCallback)(Button*);
-        typedef void (*ButtonOnPressCallback)(Button*);
-        typedef void (*ButtonOnPressStartCallback)(Button*);
-        typedef void (*ButtonOnPressEndCallback)(Button*);
+//         typedef void (*ButtonOnUpdateCallback)(Button*);
+//         typedef void (*ButtonOnhoverCallback)(Button*);
+//         typedef void (*ButtonOnPressCallback)(Button*);
+//         typedef void (*ButtonOnPressStartCallback)(Button*);
+//         typedef void (*ButtonOnPressEndCallback)(Button*);
 
-        class Button :public WidgetBase
-        {
-        public:
-            struct Callback_t
-            {
-                ButtonOnUpdateCallback onUpdate = nullptr;
-                ButtonOnhoverCallback onHover = nullptr;
-                ButtonOnPressCallback onPress = nullptr;
-                ButtonOnPressStartCallback onPressStart = nullptr;
-                ButtonOnPressEndCallback onPressEnd = nullptr;
-            };
+//         class Button :public WidgetBase
+//         {
+//         public:
+//             struct Callback_t
+//             {
+//                 ButtonOnUpdateCallback onUpdate = nullptr;
+//                 ButtonOnhoverCallback onHover = nullptr;
+//                 ButtonOnPressCallback onPress = nullptr;
+//                 ButtonOnPressStartCallback onPressStart = nullptr;
+//                 ButtonOnPressEndCallback onPressEnd = nullptr;
+//             };
 
-            struct Config_t
-            {
-                Callback_t callback;
-            };
+//             struct Config_t
+//             {
+//                 Callback_t callback;
+//             };
 
-        private:
-            Config_t _config;
+//         private:
+//             Config_t _config;
 
-        public:
-            Button() = default;
-            Button(int x, int y, int width, int height) : WidgetBase(x, y, width, height) {}
-            Button(Vector2D_t position, Vector2D_t size) : WidgetBase(position, size) {}
+//         public:
+//             Button() = default;
+//             Button(int x, int y, int width, int height) : WidgetBase(x, y, width, height) {}
+//             Button(Vector2D_t position, Vector2D_t size) : WidgetBase(position, size) {}
 
-            // Config 
-            inline const Config_t& getConfig() { return _config; }
-            inline Config_t& setConfig() { return _config; }
-            inline Callback_t& setCallback() { return _config.callback; }
-
-        protected:
-            // Core methods 
-            void update(std::uint32_t currentTime) override;
-            void hover(int x, int y) override;
-            void drag(int x, int y) override;
-            void drop() override;
-
-        protected:
-            // Internal core methods callback for derived button 
-            virtual void _onUpdate() {}
-            virtual void _onHover() {}
-            virtual void _onDrag() {}
-            virtual void _onDrop() {}
-        };
-    }
-}
+//             // Config 
+//             inline const Config_t& getConfig() { return _config; }
+//             inline Config_t& setConfig() { return _config; }
+//             inline Callback_t& setCallback() { return _config.callback; }
+//         };
+//     }
+// }
