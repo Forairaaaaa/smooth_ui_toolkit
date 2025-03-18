cbc4e9bf1cc02268a070bcf0f3a7968fcc877335
Forairaaaaa <applesyqd@outlook.com>
2025-03-18 16:51:26 +0800

    fix gcc shit

--- src/utils/easing/ease.cpp
+++ src/utils/easing/ease.cpp
@@ -11,6 +11,6 @@
 #include "ease.h"
 #include "cubic_bezier/cubic_bezier.h"
 
-auto smooth_ui_toolkit::ease_in = smooth_ui_toolkit::cubic_bezier(0.42, 0, 1, 1);
-auto smooth_ui_toolkit::ease_out = smooth_ui_toolkit::cubic_bezier(0, 0, 0.58, 1);
-auto smooth_ui_toolkit::ease_in_out = smooth_ui_toolkit::cubic_bezier(0.42, 0, 0.58, 1);
+std::function<float(float)> smooth_ui_toolkit::ease_in = smooth_ui_toolkit::cubic_bezier(0.42, 0, 1, 1);
+std::function<float(float)> smooth_ui_toolkit::ease_out = smooth_ui_toolkit::cubic_bezier(0, 0, 0.58, 1);
+std::function<float(float)> smooth_ui_toolkit::ease_in_out = smooth_ui_toolkit::cubic_bezier(0.42, 0, 0.58, 1);
