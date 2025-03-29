239e52653ad3c3f028c3c58a2a5d9642128630ed
Forairaaaaa <applesyqd@outlook.com>
2025-03-29 11:38:56 +0800

    clear dev zone

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
 v2.0.0重构中
 
-```bash
-cd example && mkdir dependencies && cd dependencies
-
-git clone -b 5.5 https://github.com/raysan5/raylib.git & 
-git clone https://github.com/Forairaaaaa/mooncake_log.git &
-git clone -b v9.2.2 https://github.com/lvgl/lvgl.git
-```
-
 todo:
 - [x] animate value default value by constructor
 - [x] https://motion.dev/docs/react-animation demo
 - [x] https://motion.dev/docs/react-animation motion+ Multicursor demo
 - [ ] easing key frame generator
-- [ ] sequence
-- [ ] baker? or fix point easing generator
+- [ ] ~~sequence~~
+- [ ] ~~baker? or fix point easing generator~~
+- [ ] color tools
+- [ ] menu shit
--- example/dev_zone.cpp
+++ example/dev_zone.cpp
@@ -8,7 +8,9 @@
  * @copyright Copyright (c) 2025
  *
  */
+// git update-index --skip-worktree example/dev_zone.cpp
 #include "utils/lvgl_wrapper.h"
+#include "utils/raylib_wrapper.h"
 #include <smooth_ui_toolkit.h>
 #include <smooth_lvgl.h>
 #include <mooncake_log.h>
@@ -16,53 +18,26 @@
 using namespace smooth_ui_toolkit;
 using namespace smooth_ui_toolkit::lvgl_cpp;
 
-int main()
+static void raylib_window()
+{
+    raylib::create_window(800, 450, "你好", [&]() {
+        // Render
+        ClearBackground(RAYWHITE);
+    });
+}
+
+static void lvgl_window()
 {
     lvgl::create_window(800, 520);
 
-    auto number_flow = new NumberFlow(lv_screen_active());
-    number_flow->setAlign(LV_ALIGN_CENTER);
-    number_flow->setPos(0, -110);
-    number_flow->setTextFont(&lv_font_montserrat_48);
-
-    auto flex_layout = new Container(lv_screen_active());
-    flex_layout->setBorderWidth(0);
-    flex_layout->setBgOpa(0);
-    flex_layout->setFlexFlow(LV_FLEX_FLOW_ROW);
-    flex_layout->setFlexAlign(LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_START);
-    flex_layout->setSize(LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-    flex_layout->setAlign(LV_ALIGN_CENTER);
-    flex_layout->setPos(0, 150);
-    flex_layout->setPadColumn(50);
-
-    auto btn_next = new Button(flex_layout->get());
-    btn_next->label().setText("-1");
-    btn_next->onClick().connect([&]() { number_flow->setValue(number_flow->value() - 1); });
-
-    auto btn_last = new Button(flex_layout->get());
-    btn_last->label().setText("+1");
-    btn_last->onClick().connect([&]() { number_flow->setValue(number_flow->value() + 1); });
-
-    auto btn_random = new Button(flex_layout->get());
-    btn_random->label().setText("random");
-    btn_random->onClick().connect([&]() { number_flow->setValue(rand()); });
-
-    int target_value = 0;
-    auto slider = new Slider(flex_layout->get());
-    slider->setRange(1, 9);
-    slider->setValue(1);
-    slider->onValueChanged().connect([&](int value) {
-        int target_value = 0;
-        for (int i = 1; i <= value; ++i) {
-            target_value = target_value * 10 + i;
-        }
-        number_flow->setValue(target_value);
-    });
-
     while (1) {
-        number_flow->update();
         lvgl::update_window();
     }
+}
 
+int main()
+{
+    raylib_window();
+    // lvgl_window();
     return 0;
 }
