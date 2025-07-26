3f1e39023beaec21753db5f6e88ce2059da203a0
Forairaaaaa <applesyqd@outlook.com>
2025-07-26 11:44:45 +0800

    add default hal implement enable

--- src/utils/hal/hal.cpp
+++ src/utils/hal/hal.cpp
@@ -9,17 +9,27 @@
  *
  */
 #include "hal.h"
+
+#ifdef SMOOTH_UI_TOOLKIT_ENABLE_DEFAULT_HAL
 #include <chrono>
 #include <thread>
+#endif
 
 using namespace smooth_ui_toolkit;
 
+#ifdef SMOOTH_UI_TOOLKIT_ENABLE_DEFAULT_HAL
 static std::chrono::steady_clock::time_point _start_time = std::chrono::steady_clock::now();
 static uint32_t _default_get_tick()
 {
     auto now = std::chrono::steady_clock::now();
     return static_cast<uint32_t>(std::chrono::duration_cast<std::chrono::milliseconds>(now - _start_time).count());
 }
+#else
+static uint32_t _default_get_tick()
+{
+    return 0; // Pass function when default HAL is disabled
+}
+#endif
 static std::function<uint32_t(void)> _custom_get_tick;
 
 void ui_hal::on_get_tick(std::function<uint32_t(void)> onGetTick)
@@ -44,10 +54,18 @@ float ui_hal::get_tick_s()
     return static_cast<float>(ms) / 1000.0f;
 }
 
+#ifdef SMOOTH_UI_TOOLKIT_ENABLE_DEFAULT_HAL
 static void _default_delay(uint32_t ms)
 {
     std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
+#else
+static void _default_delay(uint32_t ms)
+{
+    // Pass function when default HAL is disabled
+    (void)ms; // Suppress unused parameter warning
+}
+#endif
 static std::function<void(uint32_t)> _custom_delay;
 
 void ui_hal::on_delay(std::function<void(uint32_t)> onDelay)
