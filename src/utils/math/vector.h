aef94e1ea07bfcef9fc3da204c2b63462b3a1654
Forairaaaaa <applesyqd@outlook.com>
2025-08-15 23:39:10 +0800

    add vector4

--- src/utils/math/vector.h
+++ src/utils/math/vector.h
@@ -72,4 +72,72 @@ using Vector2 = Vector2Base<float>;
  */
 using Vector2i = Vector2Base<int>;
 
+/**
+ * @brief
+ *
+ */
+template <typename T>
+struct Vector4Base {
+    union {
+        struct {
+            T x;
+            T y;
+            T z;
+            T w;
+        };
+
+        struct {
+            T pos_x;
+            T pos_y;
+            T width;
+            T height;
+        };
+
+        T coord[4] = {0};
+    };
+
+    Vector4Base() : x(0), y(0), z(0), w(0) {}
+    Vector4Base(T x, T y, T z, T w) : x(x), y(y), z(z), w(w) {}
+    Vector4Base(const Vector4Base& other) : x(other.x), y(other.y), z(other.z), w(other.w) {}
+
+    Vector4Base& operator=(const Vector4Base& other)
+    {
+        if (this != &other) {
+            x = other.x;
+            y = other.y;
+            z = other.z;
+            w = other.w;
+        }
+        return *this;
+    }
+
+    void set(const T& newX, const T& newY, const T& newZ, const T& newW)
+    {
+        x = newX;
+        y = newY;
+        z = newZ;
+        w = newW;
+    }
+
+    Vector4Base clamp(const Vector4Base& pMin, const Vector4Base& pMax)
+    {
+        return Vector4Base(smooth_ui_toolkit::clamp(x, pMin.x, pMax.x),
+                           smooth_ui_toolkit::clamp(y, pMin.y, pMax.y),
+                           smooth_ui_toolkit::clamp(z, pMin.z, pMax.z),
+                           smooth_ui_toolkit::clamp(w, pMin.w, pMax.w));
+    }
+};
+
+/**
+ * @brief A 4D vector using floating-point coordinates.
+ *
+ */
+using Vector4 = Vector4Base<float>;
+
+/**
+ * @brief A 4D vector using integer coordinates.
+ *
+ */
+using Vector4i = Vector4Base<int>;
+
 } // namespace smooth_ui_toolkit
